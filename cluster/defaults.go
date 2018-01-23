abcc9c8a00b3c30773905bd4eb69ed438bc6c303
Denise Schannon <denise@rancher.com>
2018-01-23 13:25:24 -0700

    update default to k8s:v1.8.7-rancher1-1

--- cluster/defaults.go
+++ cluster/defaults.go
@@ -24,7 +24,7 @@ const (
 	DefaultServiceSidekickImage = "rancher/rke-service-sidekick:v0.1.0"
 
 	DefaultEtcdImage = "quay.io/coreos/etcd:latest"
-	DefaultK8sImage  = "rancher/k8s:v1.8.5-rancher4"
+	DefaultK8sImage  = "rancher/k8s:v1.8.7-rancher1-1"
 
 	DefaultFlannelImage    = "quay.io/coreos/flannel:v0.9.1"
 	DefaultFlannelCNIImage = "quay.io/coreos/flannel-cni:v0.2.0"
